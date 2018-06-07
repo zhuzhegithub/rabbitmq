9fe7daecc7d3d1496f5ec51f3bd9ef3f1b6518df
zhuzhe <zhe.zhu1@pactera.com>
2018-06-07 17:50:34 +0800

    配置该消费者的ack方式为手动

--- store/src/main/java/com/space/rbq/store/consumer/OrderConsumer.java
+++ store/src/main/java/com/space/rbq/store/consumer/OrderConsumer.java
@@ -36,9 +36,24 @@ public class OrderConsumer {
         try {
             // 处理消息
             System.out.println("OrderConsumer {} handleMessage :"+message);
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
             // 执行减库存操作
             storeService.update(new Gson().fromJson(new String(message.getBody()),Order.class));
+
+            /*
+            * 第一个参数 deliveryTag：就是接受的消息的deliveryTag,可以通过msg.getMessageProperties().getDeliveryTag()获得
+              第二个参数 multiple：如果为true，确认之前接受到的消息；如果为false，只确认当前消息。
+              如果为true就表示连续取得多条消息才发会确认，和计算机网络的中tcp协议接受分组的累积确认十分相似，
+              能够提高效率。
+
+              同样的，如果要nack或者拒绝消息（reject）的时候，
+              也是调用channel里面的basicXXX方法就可以了（要指定tagId）。
+
+              注意：如果抛异常或nack（并且requeue为true），消息会重新入队列，
+              并且会造成消费者不断从队列中读取同一条消息的假象。
+             */
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+
+
         }catch (Exception e){
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false,false);
         }
