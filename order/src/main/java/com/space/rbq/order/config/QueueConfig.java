13c5dd1ddc90de9ceb2086ded96fe19eea3d8209
zhuzhe <zhe.zhu1@pactera.com>
2018-06-07 16:46:31 +0800

    配置 自动删除

--- order/src/main/java/com/space/rbq/order/config/QueueConfig.java
+++ order/src/main/java/com/space/rbq/order/config/QueueConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 队列配置  可以配置多个队列
  * @author zhuzhe
@@ -16,6 +19,7 @@ public class QueueConfig {
     /*对列名称*/
     public static final String QUEUE_NAME1 = "first-queue";
     public static final String QUEUE_NAME2 = "second-queue";
+    public static final String QUEUE_NAME3 = "third-queue";
 
     @Bean
     public Queue firstQueue() {
@@ -30,5 +34,13 @@ public class QueueConfig {
     @Bean
     public Queue secondQueue() {
         return new Queue(QUEUE_NAME2,true,false,false);
+    }
+
+    @Bean
+    public Queue thirdQueue() {
+        // 配置 自动删除
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-message-ttl", 60000);//60秒自动删除
+        return new Queue(QUEUE_NAME3,true,false,true,arguments);
     }
 }
