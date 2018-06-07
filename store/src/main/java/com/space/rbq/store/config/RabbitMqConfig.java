dae4136a201ead0eeb0dcb9c54e773c113d993ae
zhuzhe <zhe.zhu1@pactera.com>
2018-06-07 18:00:51 +0800

    接收消息的格式转换器

---
+++ store/src/main/java/com/space/rbq/store/config/RabbitMqConfig.java
@@ -0,0 +1,25 @@
+package com.space.rbq.store.config;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhuzhe
+ * @date 2018/6/7 17:59
+ * @email 1529949535@qq.com
+ */
+@Configuration
+public class RabbitMqConfig {
+
+    // 接收消息的格式转换器
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+}
