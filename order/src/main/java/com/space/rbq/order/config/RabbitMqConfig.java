9c052ca4a47fefdfad3ed04da35ec20c14ed35cd
zhuzhe <zhe.zhu1@pactera.com>
2018-06-08 09:17:49 +0800

    自定义消息转换器删除

--- order/src/main/java/com/space/rbq/order/config/RabbitMqConfig.java
+++ order/src/main/java/com/space/rbq/order/config/RabbitMqConfig.java
@@ -6,7 +6,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
